f532699201812a9c6fcd0238274d5db77bb58a68
Ifycode <marychord@gmail.com>
2021-12-23 03:35:42 +0100

    Completed dicee app code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.indigo[700],
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.indigo[700],
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,110 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  //const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void randomDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+      //add 1 so that it generates only numbers btw 1 - 6
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                randomDiceFace();
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                randomDiceFace();
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+/*
+1.
+Image.asset('images/dice1.png') is another way of writing:
+
+Image(
+  image: AssetImage('images/dice1.png'),
+)
+
+
+2. FlatButton() widget is depreciated,
+therefore using TextButton() (together with padding widget)
+*/
+
+/*
+
+We have to change to stateful widget due to the state of things in our application changing.
+So we are abandoning this stateless widget class. But keeping it here as reference.
+
+//body: DicePage() is the stateless widget this time:
+class DicePage extends StatelessWidget {
+  int leftDiceNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('Pressed Left dice button');
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('Pressed Right dice button');
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+*/
